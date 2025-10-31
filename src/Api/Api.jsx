0f8e66cbcaf8dca5ab6753dbfc5ca89f02a80ed4
nabhan009 <mhdnabhan009@gmail.com>
2025-10-31 10:35:22 +0530

    dbhost

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api= axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://ecomdb2.onrender.com",
     headers: {"content-type": "application/json"},
 })
 export default api;
