713064f0b52dcdbdf864625c5d1599e1de28f26b
nabhan009 <mhdnabhan009@gmail.com>
2025-11-08 20:38:00 +0530

    Monirtor success

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api= axios.create({
-    baseURL:"https://ecomdb2.onrender.com",
+    baseURL:"https://ecomdb-3.onrender.com",
     headers: {"content-type": "application/json"},
 })
 export default api;
